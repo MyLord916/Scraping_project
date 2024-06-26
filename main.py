6d5ed83164246d4a447d1294d56f2fdeb6002ec0
mylord <frimancap@yandex.ru>
2024-06-26 12:05:47 +0500

    deep functional

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def load_page(url):
     return soup
 
 
-def get_product_pages(url):
+def get_product_pages(url) -> dict:
     soup = load_page(url)
 
     product_cards = soup.find_all('div', class_='catalog-item-content clearfix')
@@ -25,43 +25,22 @@ def get_product_pages(url):
     return products_urls
 
 
-def parse_page(content):
-    soup = load_page(content)
 
+def get_product_name(soup) -> str:
     try:
         product_name = soup.find('div', class_='catalog-detail-info__title').text
     except AttributeError:
         product_name = 'No product name'
+    return product_name
 
+def get_product_filling(soup) -> str:
     try:
         product_filling = soup.find('p').text
     except AttributeError:
         product_filling = 'No product filling'
+    return product_filling
 
-    try:
-        product_size_weight_price = (
-            soup.find_all('div', class_='catalog-detail-info__option-inner').
-            find('li', class_='catalog-detail-info__option-item').
-            find('label', class_='catalog-detail-info__option-item-text')
-        )
-        for prod in product_size_weight_price:
-            product_size = prod.text.split('/')[0].strip()
-            product_weight = prod.text.split('/')[1].replace('г', 'гр').strip()
-            product_price = prod.get('data-price') + 'р'
-            print(product_size, product_weight, product_price)
-    except AttributeError:
-        product_filling = 'No product prise'
-
-    try:
-        product_name = soup.find('div', class_='catalog-detail-info__title').text
-    except AttributeError:
-        product_name = 'No product name'
-
-    try:
-        product_filling = soup.find('p').text
-    except AttributeError:
-        product_filling = 'No product filling'
-
+def get_product_size_weight_price(soup) -> dict:
     try:
         product_size_weight_price = (
             soup.find('div', class_='catalog-detail-info__option-inner').
@@ -73,70 +52,78 @@ def parse_page(content):
             try:
                 product_size = (prod.find('label', class_='catalog-detail-info__option-item-text').
                                 text.split('/')[0].strip()).split()[0]
+                if prod.get('data-prices-group') == '1':
+                    product_size += '_cheese'
             except AttributeError:
                 product_size = None
 
             try:
                 product_weight = (prod.find('label', class_='catalog-detail-info__option-item-text').
-                                  text.split('/')[1].strip().replace('г', 'гр')).split()
+                                  text.split('/')[1].strip().split()[0])
             except AttributeError:
                 product_weight = None
 
             try:
-                product_price = (prod.find('label', class_='catalog-detail-info__option-item-text').get(
-                    'data-price') + ' р').split()
+                product_price = prod.find('label', class_='catalog-detail-info__option-item-text').get(
+                    'data-price').strip()
             except AttributeError:
                 product_price = None
-            total_info_prod_size_weight_price.append([product_size, product_weight, product_price])
-            if prod.get('data-prices-group') == '1':
-                total_info_prod_size_weight_price[el][0] = '30_cheese'
+
+            total_info_prod_size_weight_price.append({
+                product_size: {
+                    'weight': {'value': int(product_weight),
+                                'unit': 'гр'},
+                     'price':{'value': int(product_price),
+                              'unit':'р'}
+                     }})
             el += 1
-
     except AttributeError:
-        print('product size weight price not find')
+        total_info_prod_size_weight_price = None
+    return total_info_prod_size_weight_price
 
+def get_energy_value(soup) -> dict:
     try:
-        energy_value_bloc = soup.find('catalog-detail-info__energy')
         energy_value_params_bloc = (
             soup.find('div', class_='catalog-detail-info__energy').
             find_all('li', class_='catalog-detail-info__energy-list-item')
         )
-        energy_value_params = []
-        for parm in energy_value_params_bloc:
-            energy_value_params.append(parm.text.strip().split()[1:])
-    except AttributeError:
-        print('energy value bloc not find')
+        energy_value_params = {
+                'additional_info': {
+                    'proteins': None,
+                    'fats': None,
+                    'carbo': None,
+                    'calories': None
+                }}
+        count = 0
+        for el in energy_value_params['additional_info']:
+            entity_parm = energy_value_params_bloc[count].text.strip().split()[1:]
+            energy_value_params['additional_info'][el] = {
+                'value': float(entity_parm[0].replace(',', '.')),
+                'unit': entity_parm[1]}
+            count += 1
+        return energy_value_params
 
+    except AttributeError:
+        energy_value_params = None
+    return energy_value_params
+
+def get_img_url(soup):
     try:
-        url_img = (
+        img_url = (
                 web_url[:-1] +
                 soup.find('div', class_='catalog-detail').find('img').get('src')
         )
     except AttributeError:
-        url_img = 'img not find'
+        img_url = None
+    return img_url
 
-    projects_data_list.append({
-        'product_name': product_name,
-        'ingredients': product_filling,
-    })
-    for i in range(len(total_info_prod_size_weight_price)):
-        projects_data_list.append({
-            total_info_prod_size_weight_price[i][0]: {
-                'weight': {'value': total_info_prod_size_weight_price[i][1][0],
-                           'unit': total_info_prod_size_weight_price[i][1][1]},
-                'price': {'value': total_info_prod_size_weight_price[i][2][0],
-                          'unit': total_info_prod_size_weight_price[i][2][1]}
-            }})
-    projects_data_list.append({
-        'additional_info': {
-            'proteins': {'value': energy_value_params[0][0], 'unit': energy_value_params[0][1]},
-            'fats': {'value': energy_value_params[1][0], 'unit': energy_value_params[1][1]},
-            'carbo': {'value': energy_value_params[2][0], 'unit': energy_value_params[2][1]},
-            'calories': {'value': energy_value_params[3][0], 'unit': energy_value_params[3][1]}
-        },
-        'img_souce': url_img
-    })
-
+def parse_page(content) -> dict:
+    soup = load_page(content)
+    data = []
+    data.append({'product_name': get_product_name(soup), 'ingredients': get_product_filling(soup)})
+    data.append(get_product_size_weight_price(soup))
+    data.append(get_energy_value(soup))
+    return data
 
 def save_data_to_json(data: list):
     with open("projects_data.json", "w", encoding="utf-8") as file:
@@ -145,8 +132,8 @@ def save_data_to_json(data: list):
 
 def main():
     product_pages = get_product_pages(web_url + 'type1/')
-    for product_page in product_pages[:-3]:
-        parse_page(product_page)
+    for product_page in product_pages:
+        projects_data_list.append(parse_page(product_page))
     save_data_to_json(projects_data_list)
 
 
